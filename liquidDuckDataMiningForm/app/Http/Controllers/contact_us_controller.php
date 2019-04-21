1b2c8bddbcb5a21d6bcc55d4dc3942aea2837f17
lane.wheeler <lane.wheeler@egov.com>
2019-04-20 23:14:30 -0500

    got email working on the wrong port, gonna go back and check the requirements to find my next priority. I think all thats left is to send it on the right port, make the call using ajax, and style that travesty of a form template. 1:23:20 left (ps, was the hour for review counted in the 4 total? cause thats how I counted it)

--- liquidDuckDataMiningForm/app/Http/Controllers/contact_us_controller.php
+++ liquidDuckDataMiningForm/app/Http/Controllers/contact_us_controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\contact_us_model;
+use Mail;
 
 
 class contact_us_controller extends Controller
@@ -15,6 +16,18 @@ class contact_us_controller extends Controller
     public function save_contact_info(Request $request) {
     	$this->validate($request, ['name' => 'required', 'email' => 'required|email', 'company' => 'required', 'phone' => 'required', 'subject' => 'required', 'message' => 'required']);
     	contact_us_model::create($request -> all());	
+
+        Mail::send('contact_response_page', array(
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'company' => $request->get('company'),
+            'phone' => $request->get('phone'),
+            'subject' => $request->get('subject'),
+            'user_message' => $request->get('message')
+        ), function($message) use ($request) {
+             $message->from('developer-test@liquidfish.com');
+             $message->to($request->get('email'), $request->get('name'))->subject($request->get('subject'));
+        });
 
 	    return back()->with('success', 'Thanks for the message!');
     }
---
+++ liquidDuckDataMiningForm/resources/views/contact_response_page.blade.php
@@ -0,0 +1,10 @@
+<h2>Hello LiquidDuck,</h2>
+<p>You received an email from : {{ $name }}</p>
+<p>Here are the details:</p>
+<p>Name: {{ $name }}</p>
+<p>Email: {{ $email }}</p>
+<p>Company: {{ $company }}</p>
+<p>Phone: {{ $phone }}</p>
+<p>Subject: {{ $subject }}</p>
+<p>Message: {{ $user_message }}</p>
+<p>Thank You</p>
