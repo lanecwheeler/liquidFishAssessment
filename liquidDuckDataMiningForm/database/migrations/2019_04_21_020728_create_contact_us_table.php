40c465da112050faac3c6a383921e06c8d14128a
lane.wheeler <lane.wheeler@egov.com>
2019-04-20 21:13:47 -0500

    created migration. putting phone as string because european phones can have letters

---
+++ liquidDuckDataMiningForm/database/migrations/2019_04_21_020728_create_contact_us_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactUsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_us', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('email');
+            $table->string('company');
+            $table->string('phone');
+            $table->string('subject');
+            $table->string('message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_us');
+    }
+}
